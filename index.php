af5264eb2aea193a1ae99c1248279515423aa068
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-31 16:35:11 +0200

    Stampa in var_dump

--- index.php
+++ index.php
@@ -4,10 +4,22 @@
     public $language;
     public $year;
 
-    function _construct ($_title, $_language, $_year){
+    function __construct ($_title, $_language, $_year){
       $this->title = $_title;
       $this->language = $_language;
       $this->year = $_year;
     }
   }
+
+  $spider_man1 = new Movie('Spider-Man, Homecoming', 'Inglese', '2017');
+  var_dump($spider_man1);
+
+  $spider_man2 = new Movie('Spider-Man, Far From Home', 'Inglese', '2019');
+  var_dump($spider_man2);
+
+  $spider_man3 = new Movie('Spider-Man, No Way Home', 'Inglese', '2021');
+  var_dump($spider_man3);
+
+
 ?>
+
