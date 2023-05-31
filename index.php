a5898f930d70a1efb72976b48cf98825a803558a
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-05-31 15:32:52 +0200

    Costruttore

--- index.php
+++ index.php
@@ -3,5 +3,11 @@
     public $title;
     public $language;
     public $year;
+
+    function _construct ($_title, $_language, $_year){
+      $this->title = $_title;
+      $this->language = $_language;
+      $this->year = $_year;
+    }
   }
 ?>
